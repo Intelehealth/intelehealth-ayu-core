a7bef5da09ac945e63676547e8aaaec3fed43238
shaheen-p <shaheen@intelehealth.org>
2026-04-30 15:48:00 +0530

    Revert "feat: added missing files on ayu core"
    This reverts commit a23765d11cac14f0c3ee5818cf39e2aaac3dc56e.

--- src/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { ayuReducer } from './ayu.reducer';
-
-export const rootReducer = combineReducers({
-  ayu: ayuReducer,
-});
-
-export type RootState = ReturnType<typeof rootReducer>;
